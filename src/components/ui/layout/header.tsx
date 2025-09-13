8b9932dbf0ade7709b71608bac21e603bbf6e5dc
saraestefaniam <saramf240614@gmail.com>
2025-09-13 19:16:51 -0300

    Creamos el header y el icono GoodReaders. Agregamos el header a nuestro App.tsx y comentamos los estilos predeterminados en index.css que estbaan interfiriendo con nuestro diseño. Además, envolvimos nuestra App en browserrouter en main.tsx

---
+++ src/components/ui/layout/header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import GoodReadersIcon from "../icons/GoodReaders";
+import "./header.css";
+import { Link, NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/">
+        <div className="header-logo">
+          <GoodReadersIcon />
+        </div>
+      </Link>
+
+      <nav className="header-nav">
+        <NavLink to="/newuser" className="header-nav">Create user</NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
